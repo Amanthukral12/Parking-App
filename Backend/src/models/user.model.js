dab45e197a982a5e3d1edaa481f9260c5b47cb4e
Aman Thukral <thukral618@gmail.com>
2024-04-12 01:57:45 +0530

    Adding user model

---
+++ Backend/src/models/user.model.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+      lowerCase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowerCase: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    profilePhoto: {
+      type: String,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model("User", userSchema);
