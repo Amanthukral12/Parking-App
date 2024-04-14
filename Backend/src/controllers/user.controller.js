d8f61ca62f5b5b38485035caf482ef3b080e835f
Aman Thukral <thukral618@gmail.com>
2024-04-15 00:34:23 +0530

    Adding loginUser controller method and generateToken util function for access and refreshToken

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -2,6 +2,9 @@ import { User } from "../models/user.model.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { ApiError } from "../utils/ApiError.js";
+import { generateAccessAndRefreshTokens } from "../utils/generateToken.js";
+
 const registerUser = asyncHandler(async (req, res) => {
   const { fullName, email, userName, password } = req.body;
   if (
@@ -49,3 +52,48 @@ const registerUser = asyncHandler(async (req, res) => {
     .status(200)
     .json(new ApiResponse(200, createdUser, "User Created Successfully"));
 });
+
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, userName, password } = req.body;
+  if (!(userName || email)) {
+    throw new ApiError(400, "useName or email is required");
+  }
+
+  const user = User.findOne({
+    $or: [{ userName }, { email }],
+  });
+  if (!user) {
+    throw new ApiError(404, "User does not exists");
+  }
+
+  const isPasswordValid = await user.isPasswordCorrect(password);
+  if (!isPasswordValid) {
+    throw new ApiError(401, "Invalid user credentials");
+  }
+
+  const { accessToken, refreshToken } = generateAccessAndRefreshTokens(
+    user._id
+  );
+
+  const loggedInUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        { user: loggedInUser, accessToken, refreshToken },
+        "User logged in successfully"
+      )
+    );
+});
+
+export { registerUser, loginUser };
---
+++ Backend/src/utils/generateToken.js
@@ -0,0 +1,20 @@
+import { User } from "../models/user.model.js";
+import { ApiError } from "./ApiError.js";
+
+const generateAccessAndRefreshTokens = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+    user.refreshToken = refreshToken;
+    user.save({ validateBeforeSave: false });
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(
+      500,
+      "Something went wrong while generating refresh and access tokens"
+    );
+  }
+};
+
+export { generateAccessAndRefreshTokens };
