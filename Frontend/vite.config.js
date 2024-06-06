00f273448f205042fd3cc1f4a9a0dcbc6d1388eb
Aman Thukral <thukral618@gmail.com>
2024-06-06 18:28:58 +0530

    Adding support for PWA

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,5 +1,35 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+const manifestForPlugin = {
+  registerType: "autoUpdate",
+  manifest: {
+    name: "Park Saver",
+    short_name: "Park Saver",
+    description: "A Parking Saver App",
+    theme_color: "#000",
+    background_color: "#000",
+    display: "standalone",
+    scope: "/",
+    start_url: ".",
+    orientation: "portrait",
+    icons: [
+      {
+        src: "./assets/Front-car-pana.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
+      },
+      {
+        src: "./assets/Front-car-pana.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any",
+      },
+    ],
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,5 +41,6 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react()],
+  base: "./",
+  plugins: [react(), VitePWA(manifestForPlugin)],
 });
